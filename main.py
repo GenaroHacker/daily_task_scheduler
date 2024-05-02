6eda0b10806e28b56ba5ba5774f544797a907d98
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-05-02 20:21:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.director import Director
+from src.scheduler import DailyTaskScheduler
 
 if __name__ == '__main__':
     director = Director()
@@ -10,3 +11,44 @@ if __name__ == '__main__':
     director.execute_command('ls')
     director.start_new_project('Sample Project', 'Initial Setup')
     director.make_progress('Sample Project')
+
+
+if __name__ == "__main__":
+    # Define functions
+    def function_A():
+        print("Executing function A")
+
+    def function_B():
+        print("Executing function B")
+
+    def function_C():
+        print("Executing function C")
+
+    def function_D():
+        print("Executing function D")
+
+    def function_E():
+        print("Executing function E")
+
+    # Map functions to function keys
+    functions = {
+        'A': function_A,
+        'B': function_B,
+        'C': function_C,
+        'D': function_D,
+        'E': function_E
+    }
+
+    # Define weekly schedule using function keys
+    weekly_schedule = {
+        0: ['A', 'B'],  # Monday
+        1: ['C'],       # Tuesday
+        2: ['A', 'C'],  # Wednesday
+        3: ['D', 'E'],  # Thursday
+        4: ['B', 'E'],  # Friday
+        5: ['A'],       # Saturday
+        6: ['E']        # Sunday
+    }
+
+    scheduler = DailyTaskScheduler(weekly_schedule, functions)
+    scheduler.execute_instructions()
