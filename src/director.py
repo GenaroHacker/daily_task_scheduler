615047126cae159d2f2d4c15078fac29e8822149
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-05-04 00:04:55 +0200

    Update director.py

--- src/director.py
+++ src/director.py
@@ -1,13 +1,15 @@
 from src.director_modules.project_manager import ProjectDatabaseManager
 from src.director_modules.code_executor import ScriptCommandExecutor
 from src.director_modules.uihelper import UIHelper
+from src.director_modules.printer import DynamicTextPrinter
 import os
 
-class Director(ProjectDatabaseManager, ScriptCommandExecutor, UIHelper):
+class Director(ProjectDatabaseManager, ScriptCommandExecutor, UIHelper, DynamicTextPrinter):
     def __init__(self):
         ProjectDatabaseManager.__init__(self)
         ScriptCommandExecutor.__init__(self)
         UIHelper.__init__(self)
+        DynamicTextPrinter.__init__(self)
 
     def play_sound(self, sound="alarm.wav"):
         sound_path = os.path.join(self.SOUNDS_DIR, sound)
