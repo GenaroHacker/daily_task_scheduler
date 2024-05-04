fdf0d172d6f7cd81693ce465e7a9f6e4cd48933d
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-05-04 19:26:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,61 @@
 from src.director import Director
 from src.scheduler import DailyTaskScheduler
 from src.decorator import track_function
+import time
 
-if __name__ == '__main__':
-    director = Director()
-    director.clear()
-    director.sleep(seconds=0.1)
-    director.play_sound()
-    director.run_script('example.sh')
-    director.open_webpage("http://example.com")
-    director.execute_command('ls')
-    director.start_new_project('Sample Project', 'Initial Setup')
-    director.make_progress('Sample Project')
-    director.print("Hello, world! This is an example with a very long sentence to test line breaking without cutting words short. Enjoy reading slowly.\nNew lines are properly handled with adaptive delay.")
+if __name__ != '__main__':
+    d = Director()
+    #d.clear()
+    #d.sleep(seconds=0.1)
+    #d.play_sound()
+    #d.run_script('example.sh')
+    #d.open_webpage("http://example.com")
+    #d.execute_command('ls')
+    #d.start_new_project('Sample Project', 'Initial Setup')
+    #d.make_progress('Sample Project')
+    #d.print("Hello, world! This is an example with a very long sentence to test line breaking without cutting words short. Enjoy reading slowly.\nNew lines are properly handled with adaptive delay.")
     
     # Define functions
     @track_function
-    def function_A():
-        print("Executing function A")
+    def coffee():
+        current_time = time.strftime("%H:%M:%S")
+        if current_time < "07:00:00":
+            d.print(f"Good morning!\n\nIt's {time.strftime('%H:%M:%S')}.\n\nHow about a cup of coffee?")
+        elif current_time >= "07:00:00" and current_time < "10:30:00":
+            d.print(f"It's {time.strftime('%H:%M:%S')}.\n\nYou are still in time for a cup of coffee if you want.\n\nContinue with the next task in your schedule once you're coffee is ready.\n\n")
+        else:
+            d.print(f"It's {time.strftime('%H:%M:%S')}.\n\nYou should avoid drinking coffee now.\n\nIt's too late for that.\n\nContinue with the next task in your schedule instead.\n\n")
 
     @track_function
-    def function_B():
-        print("Executing function B")
+    def organize_desk():
+        d.print("Organize your desk.\n\nMake sure everything is in its place.\n\n")
 
     @track_function
-    def function_C():
-        print("Executing function C")
-
-    @track_function
-    def function_D():
-        print("Executing function D")
-
-    @track_function
-    def function_E():
-        print("Executing function E")
+    def organize_digital_files():
+        d.print("Organize your digital files.\n\n")
+        d.print("Start by cleaning up your desktop.\n\n")
+        input()
+        d.print("Now, pick one specific folder and organize its contents.\n\n")
+        input()
+        d.print("Open your email inbox and mark the spam messages by blocking the sender.\n\n")
+        input()
 
     # Map functions to function keys
     functions = {
-        'A': function_A,
-        'B': function_B,
-        'C': function_C,
-        'D': function_D,
-        'E': function_E
+        'coffee': coffee,
+        'desk': organize_desk,
+        'files': organize_digital_files
     }
 
     # Define weekly schedule using function keys
     weekly_schedule = {
-        0: ['A', 'B'],  # Monday
-        1: ['C'],       # Tuesday
-        2: ['A', 'C'],  # Wednesday
-        3: ['D', 'E'],  # Thursday
-        4: ['B', 'E'],  # Friday
-        5: ['A'],       # Saturday
-        6: ['E']        # Sunday
+        0: ['coffee'],          # Monday
+        1: ['coffee', 'desk'],  # Tuesday
+        2: ['coffee', 'files'], # Wednesday
+        3: ['coffee'],          # Thursday
+        4: ['coffee', 'desk'],  # Friday
+        5: ['coffee'],          # Saturday
+        6: ['coffee']           # Sunday
     }
 
     scheduler = DailyTaskScheduler(weekly_schedule, functions)
