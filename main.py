ad9572cb4b579309ea5abd7c996e12f9baa39fee
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-05-05 15:53:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from src.director import Director
 from src.scheduler import DailyTaskScheduler
 from src.decorator import track_function
 
@@ -9,18 +8,6 @@ from task_functions.practice import pomodoro_session
 
 
 if __name__ == '__main__':
-    #d = Director()
-    #d.clear()
-    #d.sleep(seconds=0.1)
-    #d.play_sound()
-    #d.run_script('example.sh')
-    #d.open_webpage("http://example.com")
-    #d.execute_command('ls')
-    #d.start_new_project('Sample Project', 'Initial Setup')
-    #d.make_progress('Sample Project')
-    #d.print("Hello, world! This is an example with a very long sentence to test line breaking without cutting words short. Enjoy reading slowly.\nNew lines are properly handled with adaptive delay.")
-    
-
     # Map functions to function keys
     functions = {
         'coffee': coffee,
