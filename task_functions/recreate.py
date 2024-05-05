f1291a814cdd28229e1846959efa386c66977f96
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-05-05 15:26:24 +0200

    Add files via upload

---
+++ task_functions/recreate.py
@@ -0,0 +1,13 @@
+from src.director import Director
+from src.decorator import track_function
+
+d = Director()
+#d.clear()
+#d.sleep(seconds=0.1)
+#d.play_sound()
+#d.run_script('example.sh')
+#d.open_webpage("http://example.com")
+#d.execute_command('ls')
+#d.start_new_project('Sample Project', 'Initial Setup')
+#d.make_progress('Sample Project')
+#d.print("This is an example with a very long sentence...")
