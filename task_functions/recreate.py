186b7e3e5de0f5677e6a852c92ea0b91307bc68a
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-05-07 22:28:33 +0200

    Update recreate.py

--- task_functions/recreate.py
+++ task_functions/recreate.py
@@ -1,6 +1,7 @@
 from src.director import Director
 from src.decorator import track_function
 import time
+from time import sleep
 
 d = Director()
 #d.clear()
@@ -23,4 +24,40 @@ def coffee():
         d.print(f"It's {time.strftime('%H:%M:%S')}.\n\nYou are still in time for a cup of coffee if you want.\n\nContinue with the next task in your schedule once you're coffee is ready.\n\n")
     else:
         d.print(f"It's {time.strftime('%H:%M:%S')}.\n\nYou should avoid drinking coffee now.\n\nIt's too late for that.\n\nContinue with the next task in your schedule instead.\n\n")
+    d.input()
+
+@track_function
+def breathing_exercise():
+    d.print("Welcome to the breathing exercise!\n\n")
+    sleep(1)
+    d.print("Stable, rhythmic breathing enhances RSA (Respiratory Sinus Arrhythmia), promoting cardiovascular efficiency and autonomic balance.\n\n")
+    sleep(2)
+    d.print("Sit straight on your chair, with your feet flat on the floor.\n\n")
+    sleep(2)
+    d.print("Make sure your lower back is supporting your upper body.\n\n")
+    sleep(2)
+    d.print("Keep your chest still and let your diaphragm do the work.\n\n")
+    sleep(1)
+    d.print("Press enter when you are ready to start.\n")
+    sleep(0.3)
+    d.input()
+    d.clear()
+    sleep(1)
+
+    for i in range(40):
+        d.print("Inhale")
+        d.sleep(seconds=3)
+        d.print("Hold")
+        d.sleep(seconds=1)
+        d.print("Exhale")
+        d.sleep(seconds=6)
+        d.print("Wait")
+        d.sleep(seconds=2)
+        d.clear()
+
+    d.clear()
+    d.print("You have completed the breathing exercise.\n\n")
+    sleep(1)
+    d.print("Enjoy the rest of your day!\n")
+    sleep(0.3)
     d.input()
