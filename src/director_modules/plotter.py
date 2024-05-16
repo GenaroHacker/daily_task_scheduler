4630d97f4417db625d6510c29c2e063cdb23b7ab
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-05-16 23:05:47 +0200

    Update plotter.py

--- src/director_modules/plotter.py
+++ src/director_modules/plotter.py
@@ -5,8 +5,8 @@ class Plotter:
     def __init__(self, db_path: str):
         self.db_path = db_path
 
-    def plot_time_worked(self, days: int) -> None:
-        plot_time_worked(self.db_path, days)
+    def plot_time_worked(self, days_range: int = None) -> None:
+        plot_time_worked(self.db_path, days_range)
 
     def plot_start_and_end_hours(self, days_range: int = None) -> None:
         plot_start_and_end_hours(self.db_path, days_range)
