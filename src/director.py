e12c8415c8b8be928b68799742ce0c1a1e74fa77
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-05-16 17:12:27 +0200

    Add files via upload

--- src/director.py
+++ src/director.py
@@ -2,12 +2,13 @@ from src.director_modules.project_manager import ProjectDatabaseManager
 from src.director_modules.code_executor import ScriptCommandExecutor
 from src.director_modules.uihelper import UIHelper
 from src.director_modules.printer import DynamicTextPrinter
-import os
+from src.director_modules.plotter import Plotter
 
-class Director(ProjectDatabaseManager, ScriptCommandExecutor, UIHelper, DynamicTextPrinter):
-    def __init__(self):
+class Director(ProjectDatabaseManager, ScriptCommandExecutor, UIHelper, DynamicTextPrinter, Plotter):
+    def __init__(self, events=None, days_range=30):
         ProjectDatabaseManager.__init__(self)
         ScriptCommandExecutor.__init__(self)
         UIHelper.__init__(self)
         DynamicTextPrinter.__init__(self)
-
+        Plotter.__init__(self, self.db_path)
+        
