5f4fa551a66451bd741dc8e20db3993cdcf3d823
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-05-17 12:41:10 +0200

    Update plotter.py

--- src/director_modules/plotter.py
+++ src/director_modules/plotter.py
@@ -1,5 +1,6 @@
 from src.director_modules.metrics.time_worked import plot_time_worked
 from src.director_modules.metrics.worked_hours import plot_start_and_end_hours
+from src.director_modules.metrics.time_charts import plot_task_time_distribution
 
 class Plotter:
     def __init__(self, db_path: str):
@@ -10,3 +11,7 @@ class Plotter:
 
     def plot_start_and_end_hours(self, days_range: int = None) -> None:
         plot_start_and_end_hours(self.db_path, days_range)
+
+    def plot_task_time_distribution(self) -> None:
+        plot_task_time_distribution(self.db_path)
+        
