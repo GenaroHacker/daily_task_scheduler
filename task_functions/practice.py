20e1c422cae4d1ee53ad03727e66d99082233f9d
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-05-19 01:41:09 +0200

    Update practice.py

--- task_functions/practice.py
+++ task_functions/practice.py
@@ -55,3 +55,40 @@ def pomodoro_session():
 
     d.print("Congratulations! You have completed a full Pomodoro session.\n\n")
     d.input()
+
+@track_function
+def guitar_loop_note():
+    from random import choice
+    notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+    bpms = ["30", "40", "50"]
+
+    note = choice(notes)
+    bpm = choice(bpms)
+
+    d.print("You are going to practice looping a note on the guitar.\n\n")
+    time.sleep(0.5)
+    d.print("Grab your guitar and a pick.\n\n")
+    time.sleep(0.5)
+    d.print("Tune the guitar.")
+    d.input()
+    d.clear()
+    d.print(f"Set the metronome to 5/4 time signature and {bpm} bpm.\n\n")
+    d.open_webpage("https://theonlinemetronome.com/online-metronome")
+    d.input()
+    d.clear()
+    d.print(f"The note you will practice is {note} and the metronome is set to {bpm} bpm.\n\n")
+    time.sleep(0.5)
+    d.print("The exercise consists of looping the note in each string from the fret 0 to the fret 11.\n\n")
+    time.sleep(0.5)
+    d.print("You will start from the 1st string until you reach the 6th string.\n\n")
+    time.sleep(0.5)
+    d.print("If you make a mistake, wait for the next loop to start again from the correct beat.\n\n")
+    time.sleep(0.5)
+    d.print("Use only the index finger to press the strings.\n\n")
+    time.sleep(0.5)
+    d.print("When you are ready, start the metronome and begin the exercise.\n\n")
+    time.sleep(0.5)
+    d.print("You must complete 3 loops in a row.\n\n")
+    d.input()
+
+
